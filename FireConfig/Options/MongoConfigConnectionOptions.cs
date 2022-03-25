11ef45e410266f06aaa5935ff6b93613b8a37374
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-03-25 12:30:06 +0200

    Updated option

--- FireConfig/Options/MongoConfigConnectionOptions.cs
+++ FireConfig/Options/MongoConfigConnectionOptions.cs
@@ -1,8 +1,14 @@
 ﻿namespace FireConfig.Options;
 
-public record MongoConfigConnectionOptions(
-    string ConnectionString,
-    string DatabaseName,
-    string CollectionName,
-    ICollection<string>? Keys = null
-);
+public record MongoConfigConnectionOptions
+{
+    public const string MongoConfigConnectionOptionsName = nameof(MongoConfigConnectionOptions);
+    
+    public string ConnectionString { get; set; } = default!;
+    
+    public string DatabaseName { get; set; } = default!;
+
+    public string CollectionName { get; set; } = default!;
+    
+    public ICollection<string>? Keys { get; set; } = new List<string>();
+}
