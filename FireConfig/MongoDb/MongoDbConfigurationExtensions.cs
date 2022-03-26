46ba623bc9b1cea044f4b914b9ea07a933d8dc10
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-03-26 09:51:28 +0200

    Added extension

---
+++ FireConfig/MongoDb/MongoDbConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FireConfig.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace FireConfig.MongoDb;
+
+public static class MongoDbConfigurationExtensions
+{
+    public static IConfigurationBuilder AddMongoDbConfiguration(this IConfigurationBuilder builder, MongoConfigConnectionOptions options)
+    {
+        return builder.Add(new MongoConfigurationSource(options));
+    }
+}
